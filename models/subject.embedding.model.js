2982ab7047f5413f467d97de6eddbf6b948586e1
unknown <asiralanan@gmail.com>
2025-08-26 23:59:08 +0600

    fixed an error related to topic not being saved

--- models/subject.embedding.model.js
+++ models/subject.embedding.model.js
@@ -4,7 +4,7 @@ const subjectEmbeddingSchema = new mongoose.Schema({
   subject: { englishName: { type: String, required: true }, banglaName: { type: String, required: true } },
   chapter: { englishName: { type: String, required: true }, banglaName: { type: String, required: true } },
   topicId:{ type: mongoose.Schema.Types.ObjectId, ref: 'Topic', required: true },
-  segmentUniqueKey:{uniqueKey: { type: String, required: true }, },
+  segmentUniqueKey:{ type: String, required: true },
   chunkText: { type: String, required: true },
   embedding: { type: [Number], required: true }, // embedding vector array
 });
